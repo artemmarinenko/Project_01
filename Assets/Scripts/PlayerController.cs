b696856f09e7c03f094d27237cd8ee9a9f2d9a1d
artemmarinenko671 <artemmarinenko671@gmail.com>
2021-04-04 20:14:52 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   [SerializeField]
+    private int moveSpeed = 5;
+    [SerializeField]
+    private int rotateSpeed = 300;
+    private Animator anim;
+    private Rigidbody rBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            anim.SetBool("isRunning", true);
+        }
+        else  
+        {
+            anim.SetBool("isRunning", false); ;
+        }
+
+
+        Rotate();
+        Move();
+
+
+    }
+
+    private void Move() {
+        Vector3 movement = transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        rBody.MovePosition(rBody.position + movement);
+    }
+
+    private void Rotate() {
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed);
+    }
+}
